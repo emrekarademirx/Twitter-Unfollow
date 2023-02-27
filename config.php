7308ec79735beada67805cbbd37747b45af8994e
Emre Karademir <99919329+emrekarademirx@users.noreply.github.com>
2023-02-28 01:51:09 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+// Twitter API Consumer Key
+define('CONSUMER_KEY', 'YOUR_CONSUMER_KEY');
+
+// Twitter API Consumer Secret
+define('CONSUMER_SECRET', 'YOUR_CONSUMER_SECRET');
+
+// Twitter API Access Token
+define('ACCESS_TOKEN', 'YOUR_ACCESS_TOKEN');
+
+// Twitter API Access Token Secret
+define('ACCESS_TOKEN_SECRET', 'YOUR_ACCESS_TOKEN_SECRET');
+
+// Twitter API Endpoint URL
+define('API_URL', 'https://api.twitter.com/1.1/');
+
+?>
